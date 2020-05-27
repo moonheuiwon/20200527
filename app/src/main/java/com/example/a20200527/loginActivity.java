b907c1bb69b72ebff58d8af40ffc12705e3e61ee
moonheuiwon <wonheuimoon@gmail.com>
2020-05-27 17:20:21 +0900

    서버에서 내려주는 로그인 실패 메세지를 => 토스트로 출력  - UI에 접근하려면 runOnUiThread 를 거쳐야한다.

--- app/src/main/java/com/example/a20200527/loginActivity.java
+++ app/src/main/java/com/example/a20200527/loginActivity.java
@@ -5,6 +5,7 @@ import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.a20200527.databinding.ActivityLoginBinding;
 import com.example.a20200527.databinding.ActivityLoginBinding ;
@@ -48,6 +49,15 @@ public class loginActivity extends BaseActivity {
                             }
                             else  {
                                 Log.d("분석결과", " 로그인 실패");
+                                final String failReason = json.getString("message");
+
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(mContext, failReason, Toast.LENGTH_SHORT).show();
+                                    }
+                                });
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
